5c290eb6fddbb7a5cc3a0fff6dcbf51d339096d9
lap <luuchilap@gmail.com>
2025-05-05 14:37:25 +0700

    Add

--- components/Chatbar/components/ClearConversations.tsx
+++ components/Chatbar/components/ClearConversations.tsx
@@ -49,7 +49,7 @@ export const ClearConversations: FC<Props> = ({ onClearConversations }) => {
     </div>
   ) : (
     <SidebarButton
-      text={t('Clear conversations')}
+      text={t('Clear conversationssss')}
       icon={<IconTrash size={18} />}
       onClick={() => setIsConfirming(true)}
     />
